d2933ed5a24e1819ca4e6e00e718818593c6cca6
m-nakagawa-git <mfdaitak@ce.em-net.ne.jp>
2024-03-08 11:59:52 +0900

    プロジェクトの更新: アイコン追加、URLとチャットテンプレートの変更、不要な画像の削除

--- django_chatbot/urls.py
+++ django_chatbot/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.i18n import set_language
-from chat_app.views import chat_view, ask
+from chat_app.views import chat_view
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('set_language/', set_language, name='set_language'), 
     path('chat_app/', include('chat_app.urls')),  # chat_appのurls.pyを参照
-    path('', chat_view, name='home'),             # ルートURLへのパスを追加    
+    path('', chat_view, name='home'),             # ルートURLへのパスを追加
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('icon/favicon.ico')), name='favicon'),    
 ]
